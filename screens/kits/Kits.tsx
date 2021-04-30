d0f1e25a1a185693c2688a9e2f9e27bf66a91738
Karel <bousson.karel@gmail.com>
2021-04-30 17:36:45 +0200

    get kits

--- screens/kits/Kits.tsx
+++ screens/kits/Kits.tsx
@@ -1,35 +1,66 @@
-import React from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { TouchableNativeFeedback } from 'react-native-gesture-handler';
 import Card from '../../components/Card';
+import Loading from '../../components/Loading';
+import Product from '../../models/Product';
 import { appStyle } from '../../styles/generic';
+import { createProductObject } from '../../utils/product';
+import { woocommerce } from '../../utils/WooCommerce';
 
 const Kits = ({ navigation } : any) => {
-  const detail = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [kits, setKits] = useState<Product[]>([]);
+
+  const detail = (k: Product) => {
     navigation.navigate('Kit picking');
   }
 
-  return (
+  const getKits = () => {
+    setLoading(true);
+
+    woocommerce.get.products({type: 'bundle'})
+    .then(response => {
+      const productList : Product[] = [];
+
+      for (let product of response) {
+        productList.push(createProductObject(product));
+      }
+
+      //console.log({productList});
+      setKits(productList);
+
+      // Stop the loading indicator
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    // getProducts();
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log("LOADING KITS");
+      getKits();
+    }, [])
+  );
+
+  if (loading) return ( <Loading /> )
+  else if (kits.length < 1) return (
     <View style={appStyle.container}>
-      <TouchableNativeFeedback onPress={detail}>
-        <Card type="kit" title="[MCT] Prototyping kit" sub="KIT-HW-MCT" amount="39" />
-      </TouchableNativeFeedback>
+      <Text>No kits found.</Text>
+    </View>
+  )
+  else return (
+    <View style={appStyle.container}>
 
-      <TouchableNativeFeedback onPress={detail}>
-        <Card type="kit" title="[MCT] Raspberry Pi kit" sub="KIT-HW-MCTPI" amount="39" />
+    { kits.map((k: Product) => (
+      <TouchableNativeFeedback key={k.id} onPress={() => {detail(k)}}>
+        <Card type="kit" title={k.name} sub={k.sku ? k.sku : "-"} amount={k.stock_quantity} />
       </TouchableNativeFeedback>
-
-      <TouchableNativeFeedback onPress={detail}>
-        <Card type="kit" title="[IoT] Prototyping kit" sub="KIT-HW-IOT" amount="21" />
-      </TouchableNativeFeedback>
-
-      <TouchableNativeFeedback onPress={detail}>
-        <Card type="kit" title="[Devine] Arduino kit" sub="KIT-HW-DVN" amount="50" />
-      </TouchableNativeFeedback>
-
-      <TouchableNativeFeedback onPress={detail}>
-        <Card type="kit" title="[Energiemanagement] Basiskit" sub="KIT-HW-EMG" amount="10" />
-      </TouchableNativeFeedback>
+    ))}
     </View>
   )
 }
